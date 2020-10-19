0ed3b8dfa9c0750cfa072f8e077ae140c74b3592
GDavisJ <40326457+GDavisJ@users.noreply.github.com>
2020-10-19 10:18:00 -0700

    resolve issue with push.

--- src/com/spacesta/services/ProductServiceImpl.java
+++ src/com/spacesta/services/ProductServiceImpl.java
@@ -17,21 +17,11 @@ public class ProductServiceImpl implements ProductService {
 		return productDao.registerProduct(product);
 	}
 
-<<<<<<< HEAD
-  public int registerProduct(Product product) {
-    return productDao.registerProduct(product);
-  }
- 
-public boolean checkProduct(Product product) {
-	return productDao.checkProduct(product);
-}
-=======
 
 	//used to verify if the product name already exists within the database
 	public boolean checkProduct(Product product) {
 		return productDao.checkProduct(product);
 	}
->>>>>>> 6bbad74c1783318d562a7eb881a4034aced7cff2
 
 
 	//used to get all the products from the database and return a list of products.
